639497bc9d557f3723c9fc9f15e861ae06f2f50e
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-05-08 14:14:22 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import Dex from "./Components/Dex/Dex";
 
 function App() {
+  useEffect(() => {
+    fetch("http://acnhapi.com/fish/{fishID}").then((res) => console.log(res));
+  }, []);
+
   return (
     <div className="App">
       <h1>PokeDex</h1>
